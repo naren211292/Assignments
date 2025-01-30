3c89ebb58c630beae0c769f52bbc3d6e074aef9f
naren211292 <narayanan2112@gmail.com>
2025-01-30 18:05:49 +0530

    Assignments

--- src/com/assignments/Assignment1.java
+++ src/com/assignments/Assignment1.java
@@ -4,67 +4,91 @@ public class Assignment1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-        //
+        //Temperature of a city in degrees Celsius
 		float temperature = 25.5f;
 		System.out.println(temperature);
 		
-		boolean orderState = true;
-		System.out.println(orderState);
+		//Whether a customer has placed an order: true or false
+		boolean orderState1 = true;
+		System.out.println(orderState1);
 		
+		//Whether a customer has placed an order: true or false
+		boolean orderState2 = false;
+		System.out.println(orderState2);
+		
+		//Person's phone number
 		String phoneNumber = "123-456-7890";
 		System.out.println(phoneNumber);
 		
+		//Amount of money in a customer's bank account
 		double moneyInAccount = 1000.50;
 		System.out.println(moneyInAccount);
 		
+		//Person's email address
 		String emailAddress = "john.doe@example.com";
 		System.out.println(emailAddress);
 		
+		//Coordinates of a location latitude
 		float locationLatitude = 37.7749f;
 		System.out.println(locationLatitude);
 		
+		//Coordinates of a location longitude
 		float locationLongitude = -122.4194f;
 		System.out.println(locationLongitude);
 		
+		//Person's marital status
 		boolean maritalStatus = false;
 		System.out.println(maritalStatus);
 		
+		//Person's occupation
 		String occupation = "Software Engineer";
 		System.out.println(occupation);
 		
+		//Person's favourite colour
 		String favColor = "Blue";
 		System.out.println(favColor);
 		
+		//Current year:
 		int currentYear = 2023;
 		System.out.println(currentYear);
 		
+		//Number of followers on a social media platform
 		String  followersCount = "1,000,000";
 		System.out.println(followersCount);
 		
+		//Rating of a movie:
 		float movieRating = 7.5f;
 		System.out.println(movieRating);
 		
+		//Person's blood type:
 		char bloodType = 'A';
 		System.out.println(bloodType);
 		
+		//Title of a book
 		String bookTitle = "To Kill a Mockingbird";
 		System.out.println(bookTitle);
 		
+		//Number of employees in a company
 		int employeesCount = 500;
 		System.out.println(employeesCount);
 		
+		//Time of an event
 		String eventTime = "2.30 PM";
 		System.out.println(eventTime);
 		
+		//Name of a country
 		String countryName = "United States";
 		System.out.println(countryName);
 		
+		//Person's eye color
 		String eyeColor = "Brown";
 		System.out.println(eyeColor);
 		
+		//Person's birthplace
 		String birthPlace = "New York City";
 		System.out.println(birthPlace);
 		
+		//Distance between two cities
 		float distance = 200.5f;
 		System.out.println(distance);
 		
